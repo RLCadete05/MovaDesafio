ea73c1669692ca7e99de5dc522f37c6029a6c313
RLCadete05 <r.lcadete05@gmail.com>
2022-06-12 16:50:55 -0300

    value default

--- components/Filters/FilterGroup.vue
+++ components/Filters/FilterGroup.vue
@@ -42,7 +42,7 @@ export default {
     name: 'FilterGroup',
     data () {
         return {
-            selected: null,
+            selected: "name",
             selectedName: null,
             cards: [],
 
